e8c1172ed6d7b78a9946e841feb4f04fb394f449
Tara Gamache <tgamache.13@gmail.com>
2019-09-11 12:10:44 -0700

    mvp for calculator

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React , { useState } from "react";
+import { numbers } from "../../../data";
+import  NumberButton from "./NumberButton";
 
-//import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
-//Import your array data to from the provided data file
+
+
+// // //import any components needed
+// // // example of import from data.js. Note all the ../   This is how we move through folders. 
+// // /* 
+// // import { numbers } from '../../../data' 
+// // */
+// // //Import your array data to from the provided data file
 
 const Numbers = () => {
-  // STEP 2 - add the imported data to state
+// //   // STEP 2 - add the imported data to state
+  const [numberState] = useState(numbers);
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    <div className="number_button_container" >
+     {/* STEP 3 - Use .map() to iterate over your array data and return a button
+// //        component matching the name on the provided file. Pass
+// //        it any props needed by the child component*/
+       numberState.map((numbers, index) => (
+         <NumberButton key={index} numbers={numbers} />
+       ))}
     </div>
+
   );
-};
+}
+
+export default Numbers;
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = props => {
   return (
-    <>
+    <button className ="special_button">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.specials}
+    </button>
   );
 };
+export default SpecialButton;
